f825a2fbcc5258da06fdf84ae779b8648b4fb428
tzaf <homedigsoftware@gmail.com>
2026-05-04 02:20:39 +0300

    fix all referance from raw table to dbt ref from stg table's

--- models/intermediate/workforce/int_workforce__shifts.sql
+++ models/intermediate/workforce/int_workforce__shifts.sql
@@ -1,64 +1,69 @@
 {{ config(materialized='table') }}
 
 
-WITH OrderedAttendance AS (
+with staged_attendance_events as (
+    select *
+    from {{ ref('stg_store_data__attendance_events') }}
+),
+
+OrderedAttendance AS (
     SELECT
         employee_id,
         employee_name,
-        movementtype,
-        attendancedatetime,
-        sourcesequence,
+        movement_type,
+        attendance_datetime,
+        source_sequence,
         case 
-            when movementtype in (1, 91) then  ' IN'
-            when movementtype in (2, 92) then  ' OUT'
+            when movement_type in (1, 91) then  ' IN'
+            when movement_type in (2, 92) then  ' OUT'
         else 'UNKNOWN'
             end as action_type,
         ROW_NUMBER() OVER (
             PARTITION BY employee_id
-            ORDER BY attendancedatetime
+            ORDER BY attendance_datetime
         ) AS rn
-    FROM {{ source('store_data', 'fact_attendance_event') }}
-    WHERE movementtype IN (1, 2, 91, 92)
+    FROM staged_attendance_events
+    WHERE movement_type IN (1, 2, 91, 92)
 )
 SELECT
      i.employee_id
     ,i.employee_name
 
-    ,i.attendancedatetime::date AS shiftdate
-    ,to_char(i.attendancedatetime::time, 'HH24:MI') AS shift_start_time
-    ,to_char(o.attendancedatetime::time, 'HH24:MI') AS shift_end_time
+    ,i.attendance_datetime::date AS shiftdate
+    ,to_char(i.attendance_datetime::time, 'HH24:MI') AS shift_start_time
+    ,to_char(o.attendance_datetime::time, 'HH24:MI') AS shift_end_time
 
     ,lpad(
-        (EXTRACT(EPOCH FROM (o.attendancedatetime - i.attendancedatetime))::int / 60 / 60)::text,
+        (EXTRACT(EPOCH FROM (o.attendance_datetime - i.attendance_datetime))::int / 60 / 60)::text,
         2, '0'
      ) || ':' ||
      lpad(
-        (EXTRACT(EPOCH FROM (o.attendancedatetime - i.attendancedatetime))::int / 60 % 60)::text,
+        (EXTRACT(EPOCH FROM (o.attendance_datetime - i.attendance_datetime))::int / 60 % 60)::text,
         2, '0'
      ) AS shiftduration_hhmm
 
-    ,EXTRACT(HOUR FROM i.attendancedatetime)::int AS shiftstarttour
-    ,EXTRACT(HOUR FROM o.attendancedatetime)::int AS shiftendtour
+    ,EXTRACT(HOUR FROM i.attendance_datetime)::int AS shiftstarttour
+    ,EXTRACT(HOUR FROM o.attendance_datetime)::int AS shiftendtour
 
-    ,EXTRACT(EPOCH FROM (o.attendancedatetime - i.attendancedatetime))::int / 60
+    ,EXTRACT(EPOCH FROM (o.attendance_datetime - i.attendance_datetime))::int / 60
         AS shiftdurationminutes
 
     ,ROUND(
-        (EXTRACT(EPOCH FROM (o.attendancedatetime - i.attendancedatetime)) / 3600)::numeric,
+        (EXTRACT(EPOCH FROM (o.attendance_datetime - i.attendance_datetime)) / 3600)::numeric,
         2
     ) AS shiftdurationhours
 
-    ,i.sourcesequence AS startsequence
-    ,o.sourcesequence AS endsequence
+    ,i.source_sequence AS startsequence
+    ,o.source_sequence AS endsequence
 
     ,CASE
-        WHEN i.attendancedatetime::date <> o.attendancedatetime::date
+        WHEN i.attendance_datetime::date <> o.attendance_datetime::date
         THEN 1 ELSE 0
     END AS iscrossmidnight
 
     ,CASE
-        WHEN i.movementtype IN (91, 92)
-          OR o.movementtype IN (91, 92)
+        WHEN i.movement_type IN (91, 92)
+          OR o.movement_type IN (91, 92)
         THEN 1 ELSE 0
     END AS ismanualcorrection
 
@@ -66,6 +71,6 @@ FROM OrderedAttendance i
 JOIN OrderedAttendance o
   ON i.employee_id = o.employee_id
   AND o.rn = i.rn + 1
-WHERE i.movementtype IN (1, 91)   -- IN
-  AND o.movementtype IN (2, 92)  -- OUT
+WHERE i.movement_type IN (1, 91)   -- IN
+  AND o.movement_type IN (2, 92)  -- OUT
 
